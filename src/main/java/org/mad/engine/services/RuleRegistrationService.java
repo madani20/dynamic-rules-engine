4cf25cc1c0059d8caba2e96c2119b29c31383822
Madani <khouane.madani@free.fr>
2025-06-24 17:43:44 +0200

    refactor: remove the ability to retrieve a rules file

--- src/main/java/org/mad/engine/services/RuleRegistrationService.java
+++ src/main/java/org/mad/engine/services/RuleRegistrationService.java
@@ -2,14 +2,12 @@ package org.mad.engine.services;
 
 import org.mad.engine.models.entities.RuleEntity;
 import org.mad.engine.repositories.RuleRepository;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RuleRegistrationService {
     private final RuleRepository repo;
 
-    @Value("${json.file.path}")
     private final String ruleJsonFormat;
 
     public RuleRegistrationService(RuleRepository repo, String ruleJsonFormat) {
